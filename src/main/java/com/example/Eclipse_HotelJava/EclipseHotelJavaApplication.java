a55d0a4260ba50aabbcda3d94ce02a17c9f4c56b
Jean-Jr7 <jean.jose102013@gmail.com>
2024-07-19 19:43:54 -0300

    first commit

---
+++ src/main/java/com/example/Eclipse_HotelJava/EclipseHotelJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eclipse_HotelJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipseHotelJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclipseHotelJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Eclipse_HotelJava/EclipseHotelJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eclipse_HotelJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipseHotelJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
