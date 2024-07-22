cae46425a828e55cbdcfe99c69f4529285b3ea3a
Jean-Jr7 <jean.jose102013@gmail.com>
2024-07-21 22:41:49 -0300

    Update my-custom-log

--- src/main/java/com/example/Hotel/DataLoader.java
+++ src/main/java/com/example/Hotel/DataLoader.java
@@ -14,32 +14,34 @@ import org.springframework.stereotype.Component;
 import java.sql.Timestamp;
 
 @Component
-public class DataLoader {
+public class DataLoader extends Customer {
 
     @Bean
     public CommandLineRunner loadData(RoomRepository roomRepo, CustomerRepository customerRepo, ReservationRepository reservationRepo) {
         return args -> {
 
-            Room room = new Room();
-            room.setRoomNumber(505);
-            room.setType("Executive");
-            room.setPrice(450.00);
+            /*Room room = new Room();
+            room.setRoomNumber(404);
+            room.setType("Standard");
+            room.setPrice(200.00);
             room = roomRepo.save(room);
 
             Customer customer = new Customer();
-            customer.setName("Michael Brown");
-            customer.setEmail("michael.brown@example.com");
-            customer.setPhone("5559876");
+            customer.setName("Alice Johnson");
+            customer.setEmail("alice.johnson@example.com");
+            customer.setPhone("5554321");
             customer.setCreatedAt(new Timestamp(System.currentTimeMillis()));
             customer = customerRepo.save(customer);
 
             Reservation reservation = new Reservation();
             reservation.setCustomer(customer);
             reservation.setRoom(room);
-            reservation.setCheckin(new Timestamp(System.currentTimeMillis()));
-            reservation.setCheckout(new Timestamp(System.currentTimeMillis() + 10 * 24 * 60 * 60 * 1000L));
+            reservation.setCheckin(new Timestamp(System.currentTimeMillis() + 3 * 24 * 60 * 60 * 1000L)); // 3 dias a partir de agora
+            reservation.setCheckout(new Timestamp(System.currentTimeMillis() + 10 * 24 * 60 * 60 * 1000L)); // 10 dias a partir de agora
             reservation.setStatus(ReservationStatus.SCHEDULED);
             reservationRepo.save(reservation);
+
+            */
 
             System.out.println("Dados carregados com sucesso!");
         };
